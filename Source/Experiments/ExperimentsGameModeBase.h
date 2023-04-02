cde2f97c3b291baa87eb73f0edbe7cb14bae7d2b
NolanBoukachab <32099155+NolanBoukachab@users.noreply.github.com>
2023-04-02 18:27:14 +0200

    Init files

---
+++ Source/Experiments/ExperimentsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ExperimentsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXPERIMENTS_API AExperimentsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
